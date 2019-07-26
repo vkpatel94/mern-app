05af8e714a26366508fc0be958773c07e2a62007
Vaikunth Patel <vaikunth.patel94@gmail.com>
2019-07-26 13:12:45 -0400

    mongo update

---
+++ server/config/prod.js
@@ -0,0 +1,8 @@
+// Credentials for prod
+
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    databaseURI: process.env.MONGO_URI,
+    cookieKey: process.env.COOKIE_KEY
+};
